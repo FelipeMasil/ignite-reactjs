fbb49c23ebc1c93df0ba964abcb9351360dfb527
FelipeMasil <felipe.manoel.silva@gmail.com>
2022-08-04 20:59:53 -0300

    01 - Sidebar completo

--- 01-ignite_feed/src/components/Sidebar.jsx
+++ 01-ignite_feed/src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles  from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -7,12 +9,15 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
 
             <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/FelipeMasil.png" />
+
                 <strong>Felipe Manoel</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
                 <a href="#">
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
